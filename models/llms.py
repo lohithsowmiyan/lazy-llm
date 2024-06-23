f052608ac64223a6f9593b2fe9280303cb82581e
lohith sowmiyan <lohithsowmiyan@lohiths-MacBook-Pro.local>
2024-06-22 20:37:01 -0400

    added models

--- models/llms.py
+++ models/llms.py
@@ -1,5 +1,9 @@
+import os
 import torch
+import getpass
 from langchain_huggingface.llms import HuggingFacePipeline
+from langchain_openai import OpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
 from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline, BitsAndBytesConfig
 
 class LLM:
@@ -14,18 +18,36 @@ class LLM:
         self.top_p = top_p
     
     def __repr__(self):
-        return {"model" : self.model_name,
-         "temperature" : self.temperature, "max token" : self.max_tokens,
-          "top p" : self.top_p}
-
-
+        return f'LLM(model = {self.model_name}, temperature = {self.temperature}, max token = {self.max_tokens}, top p = {self.top_p})'
+    
 class Local_LLM(LLM):
+    """
+
+    """
     def __init__(self, model_name : str, temperature : float, max_tokens : int, top_p : float, **kwargs):
         super().__init__(model_name, temperature, max_tokens, top_p)
         self.model = AutoModelForCausalLM.from_pretrained(self.model_name)
         self.tokenizer = AutoTokenizer.from_pretrained(self.model_name)
+        self.quantization = kwargs.get('quantization', False)
+        self.quant_bits = kwargs.get('nbits', 8)
+        
+        if self.quantization and self.quant_bits == 4:
+            self.bnb_config = BitsAndBytesConfig(
+                load_in_4bit = True,
+                bnb_4bit_use_double_quant = True,
+                bnb_4bit_quant_type="nf4",
+                bnb_4bit_compute_dtype = torch.bfloat16
+            )
+        
+        elif self.quantization and self.quant_bits == 8:
+            self.bnb_config = BitsAndBytesConfig(
+                load_in_8bit=True,
+                bnb_8bit_use_double_quant=True,
+                bnb_8bit_quant_type="int8",
+                bnb_8bit_compute_dtype=torch.bfloat16  
+            )
 
-    def get_model():
+    def get_model() -> HuggingFacePipeline:
         pipe = pipeline(
                "text-generation",
                 model= self.model,
@@ -36,6 +58,47 @@ class Local_LLM(LLM):
                )
 
         return HuggingFacePipeline(pipeline = pipe)
+
+    def get_model_with_quantization() -> HuggingFacePipeline:
+        if not self.quantization:
+            raise Exception("Your model does not have necessary quantization config setup")
+        self.model = AutoModelForCausalLM.from_pretrained(self.model_name, quantization_config = self.bnb_config)
+        return get_model()
+
+
+class API_LLM(LLM):
+    """
+
+    """
+    def __init__(self, model_name : str, temperature : float, max_tokens : int, top_p : float, **kwargs):
+        super().__init__(model_name, temperature, max_tokens, top_p)
+
+    def get_model():
+        if "gemini" in self.model_name:
+            if "GOOGLE_API_KEY" not in os.environ:
+                print("Unable to find the API key please enter here:")
+                os.environ["GOOGLE_API_KEY"] = getpass.getpass()
+            return ChatGoogleGenerativeAI(
+                        model= self.model_name
+                        temperature= self.temperature,  
+                        max_tokens= self.max_tokens, 
+                        top_p= self.top_p
+                    )
+
+        elif "gpt" in self.model_name:
+            if "OPENAI_API_KEY" not in os.environ:
+                print("Unable to find the API key please enter here:")
+                os.environ["OPENAI_API_KEY"] = getpass.getpass()
+            return OpenAI(
+                        model= self.model_name
+                        temperature= self.temperature,  
+                        max_tokens= self.max_tokens, 
+                        top_p= self.top_p
+                )
+
+
+        
+
 
 
 
