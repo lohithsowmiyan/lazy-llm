69a3a72e9cc7ced08fc29ac9df3ab58a75ed28f6
Lohith Sowmiyan Panjalingapuram Senthilkumar <lpanjal@vclvm178-152.vcl.ncsu.edu>
2024-07-03 18:12:25 -0400

    r vanilla

--- lazy.py
+++ lazy.py
@@ -9,56 +9,56 @@ import warnings
 import time
 
 
-def vanilla(args):
-    warnings.filterwarnings("ignore")
-    model = load_model(args).get_model_with_quantization() if args.quantization else load_model(args).get_model()
+# def vanilla(args):
+#     warnings.filterwarnings("ignore")
+#     model = load_model(args).get_model_with_quantization() if args.quantization else load_model(args).get_model()
 
-    def _tile(lst):
-        num = adds(NUM(),lst)
-        n=100
-        print(f"{len(lst):5} : {num.mu:5.3} ({num.sd:5.3})",end="")
-        sd=int(num.sd*n/2)
-        mu=int(num.mu*n)
-        print(" "*(mu-sd), "-"*sd,"+"*sd,sep="")
+#     def _tile(lst):
+#         num = adds(NUM(),lst)
+#         n=100
+#         print(f"{len(lst):5} : {num.mu:5.3} ({num.sd:5.3})",end="")
+#         sd=int(num.sd*n/2)
+#         mu=int(num.mu*n)
+#         print(" "*(mu-sd), "-"*sd,"+"*sd,sep="")
 
-    def learner(i:data, callBack=lambda x:x):
-        """
+#     def learner(i:data, callBack=lambda x:x):
+#         """
         
-        """
-        def _ranked(lst:rows) -> rows:
-            "Sort `lst` by distance to heaven. Called by `_smo1()`."
-            lst = sorted(lst, key = lambda r:d2h(i,r))
-            _tile([d2h(i,r) for r in lst])
-            # print(d2h of the best row)
-            return lst
+#         """
+#         def _ranked(lst:rows) -> rows:
+#             "Sort `lst` by distance to heaven. Called by `_smo1()`."
+#             lst = sorted(lst, key = lambda r:d2h(i,r))
+#             _tile([d2h(i,r) for r in lst])
+#             # print(d2h of the best row)
+#             return lst
 
-        def llm_guesser(current: row, done: rows) -> row:
-            cut = int(.5 + len(done) ** 0.5)
-            best = clone(i, done[:cut]).rows
-            rest = clone(i, done[cut:]).rows
-            prompt = load_prompt[args.dataset].getZeroShot(best, rest)
-            chain = prompt | model
-            output = chain.invoke({"input": f"{current[:len(i.cols.x)]}"}) # data.cols.x
-            print(output)
-            if "best" in output.lower(): return current
-            return None
+#         def llm_guesser(current: row, done: rows) -> row:
+#             cut = int(.5 + len(done) ** 0.5)
+#             best = clone(i, done[:cut]).rows
+#             rest = clone(i, done[cut:]).rows
+#             prompt = load_prompt[args.dataset].getZeroShot(best, rest)
+#             chain = prompt | model
+#             output = chain.invoke({"input": f"{current[:len(i.cols.x)]}"}) # data.cols.x
+#             print(output)
+#             if "best" in output.lower(): return current
+#             return None
             
         
-        def _smo1(todo:rows, done:rows) -> rows:
-            "Guess the `top`  unlabeled row, add that to `done`, resort `done`, and repeat"
-            for k in todo:
-                if len(done) > 30: break
-                top = llm_guesser(k, done)
-                if(top == None): continue
-                print(d2h(i,top))
-                done += [top]
-                done = _ranked(done)
-            return done
+#         def _smo1(todo:rows, done:rows) -> rows:
+#             "Guess the `top`  unlabeled row, add that to `done`, resort `done`, and repeat"
+#             for k in todo:
+#                 if len(done) > 30: break
+#                 top = llm_guesser(k, done)
+#                 if(top == None): continue
+#                 print(d2h(i,top))
+#                 done += [top]
+#                 done = _ranked(done)
+#             return done
 
-        random.shuffle(i.rows)
-        return _smo1(i.rows[4:], _ranked(i.rows[:4]))
+#         random.shuffle(i.rows)
+#         return _smo1(i.rows[4:], _ranked(i.rows[:4]))
 
-    learner(DATA(csv(args.dataset)), _tile)
+#     learner(DATA(csv(args.dataset)), _tile)
 
 def vanilla1(args):
     warnings.filterwarnings("ignore")
